5edf49a4d153d40f1ed47e59f745ba7f0606f325
Jay <jason_milton@hotmail.com>
2020-03-24 13:35:31 -0400

    fixed nav but need to fix js on scroll. jumbotron image blur, basic layout almost done - needs image gallery..

--- script.js
+++ script.js
@@ -1,33 +1,6 @@
-// const nav1 = document.querySelector('.nav1');
-
-// function hideMenu() {
-//   const menu = document.querySelector('.nav1');
-//   menu.style.display = 'none';
-// }
-// nav1.addEventListener('click', hideMenu);
-
-// window.addEventListener('scroll', function() {
-//   if (window.scroll()) {
-//     nav.addClass('black');
-//   } else {
-//     nav.removeClass('black');
-//   }
-// });
-
-// $(window).on('scroll', function() {
-//   if ($(window).scrollTop()) {
-//     $('nav').addClass('black');
-//   } else {
-//     $('nav').removeClass('black');
-//   }
-// });
-
-// const navBar = document.querySelector('.navbar');
-// const scrollTop = window.pageYOffset || document.documentElement.scrollTop;
-
-// function changeColorOnScroll() {
-//   if (navBar.documentElement.scrollTop >= 15) {
-//     navBar.style.backgroundColor = 'black';
-//   }
-// }
-// window.addEventListener('scroll', changeColorOnScroll);
+$(function() {
+  $(document).scroll(function() {
+    const $nav = $('.fixed-top');
+    $nav.toggleClass('scrolled', $(this).scrollTop() > $nav.height());
+  });
+});
