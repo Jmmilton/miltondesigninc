0322e4742131dee2884055a2db6aa19c4f34b18f
Jay <jason_milton@hotmail.com>
2020-03-24 16:40:02 -0400

    fixed stickynav, fixed a few other things..

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
-$(function() {
-  $(document).scroll(function() {
-    const $nav = $('.fixed-top');
-    $nav.toggleClass('scrolled', $(this).scrollTop() > $nav.height());
-  });
-});
+const navbar = document.querySelector('.navbar');
+
+window.onscroll = function() {
+  // pageYOffset or scrollY
+  if (window.pageYOffset > 265) {
+    navbar.classList.add('scrolled');
+  } else {
+    navbar.classList.remove('scrolled');
+  }
+  console.log(window);
+};
