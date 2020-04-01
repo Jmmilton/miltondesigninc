3bd544614a39c2bf4e12170441480869a7c13aac
Jay <jason_milton@hotmail.com>
2020-04-01 16:31:50 -0400

    js cache fix..

--- script.js
+++ script.js
@@ -8,3 +8,32 @@ window.onscroll = function() {
     navbar.classList.remove('scrolled');
   }
 };
+
+// Cache Buster
+
+(function() {
+  const rep = /.*\?.*/;
+  const links = document.getElementsByTagName('link');
+  const scripts = document.getElementsByTagName('script');
+  const process_scripts = false;
+  for (var i = 0; i < links.length; i++) {
+    const link = links[i];
+    const { href } = link;
+    if (rep.test(href)) {
+      link.href = `${href}&${Date.now()}`;
+    } else {
+      link.href = `${href}?${Date.now()}`;
+    }
+  }
+  if (process_scripts) {
+    for (var i = 0; i < scripts.length; i++) {
+      const script = scripts[i];
+      const { src } = script;
+      if (rep.test(src)) {
+        script.src = `${src}&${Date.now()}`;
+      } else {
+        script.src = `${src}?${Date.now()}`;
+      }
+    }
+  }
+})();
