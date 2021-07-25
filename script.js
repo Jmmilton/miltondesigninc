057ec9e2157e093b4eb07e186fb88f06c97e5ee1
Jmmilton <jason_milton@hotmail.com>
2021-07-24 22:37:21 -0400

    mobile bg img jump

--- script.js
+++ script.js
@@ -47,3 +47,13 @@ $('body').on('click', function() {
 })();
 
 localStorage.clear();
+
+
+var bg = $("#bg1, #bg2");
+
+function resizeBackground() {
+  bg.height( $(window).height() + 60);
+}
+
+$(window).resize(resizeBackground);
+resizeBackground();
